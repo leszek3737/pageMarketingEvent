1b91257f635d85eae9249d2335d158c3c9bda264
Unknown <leszek@lubcon.org>
2018-01-10 10:55:34 +0100

    Add init files

---
+++ tasks/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    glob: {
+        src: "./src/**/*",
+        scss: './src/**/*.scss',
+        js: './src/**/*.js',
+        html: './src/**/*.html',
+        fonts: './src/fonts/**/*',
+        fontsLib: './../psdToHtml/dpsn-page/node_modules/bootstrap-sass/assets/fonts/bootstrap/**/*',
+        img: './src/img/**/*',
+    },
+    entryPoint: {
+        scss: './src/scss/style.scss'
+    },
+    path: {
+        css: './dist/css/',
+        js: './dist/js/',
+        dist: './dist/',
+        fonts: './dist/fonts/',
+        fontsLib: './dist/fonts/bootstrap/',
+        vector: './dist/img/',
+        img: './dist/img/',
+    },
+    concatOrder: {
+        js: './src/**/*.js',
+        jsLib: [
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js",
+        ]
+    },
+    name: {
+        js: "app.js",
+        jsMin: "app.min.js",
+        jsMinLib: "assets.min.js",
+        css: "app.css",
+        cssMin: "app.min.css",
+    }
+};
